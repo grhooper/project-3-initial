7c83b9c41806e5bff4a357c6508f185a98d6e16a
grhooper <grant.hooper886@gmail.com>
2023-10-26 11:35:50 -0700

    project 3 w/ extra credit

--- src/com/example/iterable/Bag.java
+++ src/com/example/iterable/Bag.java
@@ -2,6 +2,8 @@ package com.example.iterable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 public class Bag<T> implements Container<T> {
     private List<T> items;
@@ -29,4 +31,16 @@ public class Bag<T> implements Container<T> {
     public Iterator<T> iterator() {
         return items.iterator();
     }
+
+    // I'm not sure on this one, did some googling and found some people had something like this
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        items.forEach(action);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return items.spliterator();
+    }
+
 }
